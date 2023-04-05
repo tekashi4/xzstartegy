203c8508a4f7062175e0aafe84cdc9ebc47d150c
Tekashi <66803636+tekashi4@users.noreply.github.com>
2023-04-05 13:34:37 -0300

    Delete background.js

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
-    'bounds': {
-      'width': 485,
-      'height': 480
-    },alwaysOnTop: true
-  });
-
-});
